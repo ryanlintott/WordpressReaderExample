c08269d95f7065f6436a3e8fadc0efd1558a23fa
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-05-18 15:00:18 -0400

    Initial Commit

---
+++ WordpressReaderExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordpressReaderExample
+//
+//  Created by Ryan Lintott on 2021-05-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
